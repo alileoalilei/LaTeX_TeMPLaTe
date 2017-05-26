0ea90e5064295d0c07ee0b5c865e6914c39c01fe
alileoalilei <aydogdu.ila@gmail.com>
2017-05-26 21:24:42 +0200

    rename reorganized

--- rename_template.sh
+++ rename_template.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+if [ $1 = 'BEAMER' ]; then
 pdate='a' # i.e. 201705
 pname='b' # i.e. Meeting_City
 pauth='c' # i.e. surname
@@ -16,4 +17,5 @@ sed 's/SuNuM_TeMPLaTe_main/'${ptemp}'_main/' \
 cat SuNuM_TeMPLaTe_main.tex > ${ptemp}_main.tex
 #cat SuNuM_TeMPLaTe.bib > ${sunum_template}.bib
 #rm sunum_template.tex sunum_template_main.tex sunum_template.bib
+fi
 exit
