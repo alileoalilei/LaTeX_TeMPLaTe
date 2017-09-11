dccb253d5a61754040bd9b2a99c63f3bb91a0cce
alileoalilei <aydogdu.ila@gmail.com>
2017-09-11 12:53:49 +0200

    added ABSTRACT part

--- rename_template.sh
+++ rename_template.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+if [ $1 == "BEAMER" ] then
 pdate='a' # i.e. 201705
 pname='b' # i.e. Meeting_City
 pauth='c' # i.e. surname
@@ -14,6 +15,20 @@ sed 's/SuNuM_TeMPLaTe_main/'${ptemp}'_main/' \
          SuNuM_TeMPLaTe.tex > ${ptemp}.tex
 
 cat SuNuM_TeMPLaTe_main.tex > ${ptemp}_main.tex
-#cat SuNuM_TeMPLaTe.bib > ${sunum_template}.bib
-#rm sunum_template.tex sunum_template_main.tex sunum_template.bib
-exit
+fi
+if [ $1 == "ABSTRACT" ] then
+pdate='a' # i.e. 201705
+pname='b' # i.e. Meeting_City
+pauth='c' # i.e. surname
+ptemp=${pdate}_${pname}_${pauth}
+
+mv ABSTRACT_TeMPLaTe ${ptemp}; cd ${ptemp}
+#
+sed -e 's/ABSTRACT_TeMPLaTe/'${ptemp}'/' \
+         Makefile.template  > Makefile
+
+sed 's/ABSTRACT_TeMPLaTe_main/'${ptemp}'_main/' \
+         ABSTRACT_TeMPLaTe.tex > ${ptemp}.tex
+
+cat ABSTRACT_TeMPLaTe_main.tex > ${ptemp}_main.tex
+fi
